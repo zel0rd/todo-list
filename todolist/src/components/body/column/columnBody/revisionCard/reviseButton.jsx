9ac82150135b46222b5629cb06f2d5961ab0a36c
Seohyoung <prophecy803@gmail.com>
2021-04-14 20:33:09 +0900

    [FIX] type

--- todolist/src/components/body/column/columnBody/revisionCard/reviseButton.jsx
+++ todolist/src/components/body/column/columnBody/revisionCard/reviseButton.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { ButtonAccent, ButtonAccentText } from '../../../../style/button-accent.jsx';
+import { getData, patchData, postData, getRandomUser } from '../../../../../utils/axios.js';
 
 const ReviseButton = props => {
+  const postModifiedData = async () => {
+    const url = `http://localhost:3002/column/${props.id}/id`;
+    const foo = await getData(url);
+    console.log(foo);
+  };
+
   return (
-    <ButtonAccent className="revise-card-cancel-button-section">
+    <ButtonAccent onClick={postModifiedData} className="revise-card-cancel-button-section">
       <ButtonAccentText className="revise-card-cancel-button">수정</ButtonAccentText>
     </ButtonAccent>
   );
