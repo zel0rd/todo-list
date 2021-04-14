6e599bebfeaeb075cc298f2b0a9b60ef6b12e060
younggil <zelord.kwoun@gmail.com>
2021-04-14 17:55:38 +0900

    [ADD] fab eventFunction add

--- todolist/src/components/body/fabButton/fabButton.jsx
+++ todolist/src/components/body/fabButton/fabButton.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { AddIcon, Button } from './fabButton.style';
+import { postData } from '../../../utils/axios.js'
+
+// {
+//     "title": "새 컬럼"
+// }
+
+const card = {
+  "columnTitle": "새 컬럼",
+  "cards": [
+    {
+      "cardTitle": "adfaf",
+      "cardContents": "asdfasdffads"
+    },
+    {
+      "cardTitle": "git공부",
+      "cardContents": "merge공부\npush공부\npull공부"
+    }
+  ]
+}
+
+const addNewColumnEvent = () =>{
+  const columnUrl = "http://localhost:3002/column/";
+  postData(columnUrl, card);
+}
 
 const FabButton = () => {
   return (
-    <Button className="fab-button-section">
+    <Button className="fab-button-section" onClick={addNewColumnEvent}>
       <AddIcon />
     </Button>
   );
