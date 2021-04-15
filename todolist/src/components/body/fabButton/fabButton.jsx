6a81a5d7aa28eb3f25c39b6ffb9e11786231a546
younggil <zelord.kwoun@gmail.com>
2021-04-15 10:16:24 +0900

    [ADD] Column Create Event Add

--- todolist/src/components/body/fabButton/fabButton.jsx
+++ todolist/src/components/body/fabButton/fabButton.jsx
@@ -2,32 +2,21 @@ import React from 'react';
 import { AddIcon, Button } from './fabButton.style';
 import { postData } from '../../../utils/axios.js'
 
-// {
-//     "title": "새 컬럼"
-// }
-
 const card = {
   "columnTitle": "새 컬럼",
   "cards": [
-    {
-      "cardTitle": "adfaf",
-      "cardContents": "asdfasdffads"
-    },
-    {
-      "cardTitle": "git공부",
-      "cardContents": "merge공부\npush공부\npull공부"
-    }
   ]
 }
 
-const addNewColumnEvent = () =>{
+const addNewColumnEvent = (getColumnData) =>{
   const columnUrl = "http://localhost:3002/column/";
   postData(columnUrl, card);
+  getColumnData()
 }
 
-const FabButton = () => {
+const FabButton = ({getColumnData}) => {
   return (
-    <Button className="fab-button-section" onClick={addNewColumnEvent}>
+    <Button className="fab-button-section" onClick={() => addNewColumnEvent(getColumnData)}>
       <AddIcon />
     </Button>
   );
